bf6cd2dd34cf37a6d337a3861dcdd420faa8ac4f
Vincent (Anapher) <anapher@protonmail.com>
2023-01-23 18:31:04 +0100

    fix some stuff

--- app/src/features/game/hooks/useCardsFromDecks.ts
+++ app/src/features/game/hooks/useCardsFromDecks.ts
@@ -3,13 +3,13 @@ import niceDeck from '../../../assets/card-decks/nice.json';
 import { GameCardData } from '../../../card-schemas';
 
 const decks: { [key: string]: GameCardData[] } = {
-   niceDeck,
+   nice: niceDeck,
 } as any;
 
 export default function useCardsFromDecks(deckIds?: string[]): { cards: GameCard[]; error?: string; loading: boolean } {
    return {
       cards: Object.entries(decks)
-         .filter((x) => true)
+         .filter(([id]) => deckIds?.includes(id))
          .flatMap<GameCard>(([id, cards]) => cards.map<GameCard>((data, index) => ({ id: `${id}/${index}`, data }))),
       loading: false,
    };
