5adcdea57b757c0e529dcc6f72897cec517d7cb2
Vincent (Anapher) <anapher@protonmail.com>
2023-01-27 14:03:46 +0100

    add shivani cards

--- app/src/assets/card-decks/index.ts
+++ app/src/assets/card-decks/index.ts
@@ -1,6 +1,20 @@
 import nice from './nice.json';
 import bullshitfacts from './bullshitfacts.json';
 import horny from './horny.json';
+import shivani_minigame from './shivani_minigame.json';
+import shivani_mostlikely from './shivani_mostlikely.json';
+import shivani_neverhaveiever from './shivani_neverhaveiever.json';
+import shivani_categories from './shivani_categories.json';
 import { GameCardData } from '../../card-schemas';
 
-export default { nice, bullshitfacts, horny } as { [key: string]: GameCardData[] };
+export default {
+   nice,
+   bullshitfacts,
+   horny,
+   shivani_minigame,
+   shivani_mostlikely,
+   shivani_neverhaveiever,
+   shivani_categories,
+} as {
+   [key: string]: GameCardData[];
+};
